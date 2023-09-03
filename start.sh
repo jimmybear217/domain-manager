6a1f9e733f78374b553da380d57ac0e975097e6f
Jimmy Remy <jimmy@itbyjimmy.com>
2023-09-03 11:07:36 -0700

    created start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/domain-manager/src
+git fetch
+python3 -m pip install -r requirements.txt
+echo "Existing Processes: $(ps -x | grep "python3 main.py" | grep -v "grep")"
+if [ $(ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | wc -l) -gt 0 ]; then
+        echo "Stopping Process: $(ps -x | grep "python3 main.py" | grep -v "grep")"
+        ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | xargs kill
+fi
+echo "Starting Process"
+python3 main.py &> ./stdout 2> ./stderr &
+cat ./stderr
+tail -f ./stdout
