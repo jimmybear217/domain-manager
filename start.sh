6c6639843c861bd3bb8086b9dbe9b75984ff562e
Jimmy Remy <jimmy@itbyjimmy.com>
2023-09-03 13:28:24 -0700

    added delay in script

--- start.sh
+++ start.sh
@@ -2,11 +2,13 @@
 cd ~/domain-manager/src
 git pull
 python3 -m pip install -r requirements.txt
+sleep 1
 echo "Existing Processes: $(ps -x | grep "python3 main.py" | grep -v "grep")"
 if [ $(ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | wc -l) -gt 0 ]; then
         echo "Stopping Process: $(ps -x | grep "python3 main.py" | grep -v "grep")"
         ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | xargs kill
 fi
+sleep 1
 echo "Starting Process"
 python3 main.py &> ./stdout 2> ./stderr &
 cat ./stderr
