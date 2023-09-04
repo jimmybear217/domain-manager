2e3e8986b11f7e63c7cb165840e25a2b6322d893
Jimmy Remy <jimmy@itbyjimmy.com>
2023-09-03 17:51:00 -0700

    updated kill line

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ git reset --hard HEAD
 git pull
 python3 -m pip install -r src/requirements.txt
 sleep 1
-echo "Existing Processes: $(ps -x | grep "python3 main.py" | grep -v "grep")"
-if [ $(ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | wc -l) -gt 0 ]; then
-        echo "Stopping Process: $(ps -x | grep "python3 main.py" | grep -v "grep")"
-        ps -x | grep "python3 main.py" | grep -v "grep" | awk '{print $1}' | xargs kill
+echo "Existing Processes: $(ps -x | grep "python3 src/main.py" | grep -v "grep")"
+if [ $(ps -x | grep "python3 src/main.py" | grep -v "grep" | awk '{print $1}' | wc -l) -gt 0 ]; then
+        echo "Stopping Process: $(ps -x | grep "python3 src/main.py" | grep -v "grep")"
+        ps -x | grep "python3 src/main.py" | grep -v "grep" | awk '{print $1}' | xargs kill
 fi
 sleep 1
 echo "Starting Process"
